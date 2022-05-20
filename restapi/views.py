db3487615662c073fe8c898a75354dfd41cc53b9
Shivam-TI <shivam.gupta@trilogy.com>
2022-05-20 19:07:55 +0000

    Fixed wrong funtion call

--- restapi/views.py
+++ restapi/views.py
@@ -189,7 +189,7 @@ def log_processor(request):
     if len(log_files) == 0:
         return Response({"status": "failure", "reason": "No log files provided in request"},
                         status=status.HTTP_400_BAD_REQUEST)
-    logs = multiThreadedReader(urls=data['logFiles'], num_threads=data['parallelFileProcessingCount'])
+    logs = multi_threaded_reader(urls=data['logFiles'], num_threads=data['parallelFileProcessingCount'])
     sorted_logs = sort_by_time_stamp(logs)
     cleaned = transform(sorted_logs)
     data = aggregate(cleaned)
