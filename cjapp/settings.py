474599c4c032abc3a1be64a24391882f3da4a918
Shivam-TI <shivam.gupta@trilogy.com>
2022-05-20 13:14:20 +0000

    Added database_name and debug_flag to .env file

--- cjapp/settings.py
+++ cjapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 # Application definition
 
@@ -119,7 +119,7 @@ WSGI_APPLICATION = 'cjapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': config('DATABASE_NAME')),
     }
 }
 
